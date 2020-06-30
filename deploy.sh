ecab93a303e47684c8d8037ff5b758dc22c13a16
sgg <sgg@163.com>
2020-06-30 11:54:44 +0800

    chore: change deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -e
 
 # 生成静态文件
 #npm run docs:build
-yarn prod
+yarn pro
 
 # 进入生成的文件夹
 cd blogs/.vuepress/dist
@@ -13,9 +13,9 @@ cd blogs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
